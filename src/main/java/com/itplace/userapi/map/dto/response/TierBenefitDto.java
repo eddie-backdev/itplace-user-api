433fce337cae00f53d831e6ca63dabfac3954f84
Eddie <devleehy@gmail.com>
2026-03-22 01:21:50 +0900

    fix: TierBenefitDto에 Jackson 역직렬화를 위한 생성자 추가
    Redis 캐시에서 TierBenefitDto를 역직렬화할 때 Jackson이 기본 생성자를
    필요로 함. 기존에는 @Builder만 있어 인자 없는 생성자가 없었으므로
    역직렬화 시 오류 발생.
    
    - @NoArgsConstructor 추가: Jackson 역직렬화 시 기본 생성자 사용
    - @AllArgsConstructor 추가: @Builder와 함께 사용하는 전체 인자 생성자 명시

--- src/main/java/com/itplace/userapi/map/dto/response/TierBenefitDto.java
+++ src/main/java/com/itplace/userapi/map/dto/response/TierBenefitDto.java
@@ -1,13 +1,17 @@
 package com.itplace.userapi.map.dto.response;
 
 import com.itplace.userapi.benefit.entity.enums.Grade;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TierBenefitDto {
     private Grade grade;
     private String context;
