4054947d03475355c9427df230c67e65d3870c32
Eddie <devleehy@gmail.com>
2026-04-18 23:35:29 +0900

    refactor: DataSourceConfig에서 중복 @EnableJpaRepositories 제거

--- src/main/java/com/itplace/userapi/common/db/DataSourceConfig.java
+++ src/main/java/com/itplace/userapi/common/db/DataSourceConfig.java
@@ -10,12 +10,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableJpaRepositories
 public class DataSourceConfig {
 
     // Write replica 정보로 만든 DataSource
